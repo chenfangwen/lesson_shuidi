0f67024a89bffacd55ede6e385cce2eda881f6eb
chenfangwen <2325189133@qq.com>
2020-02-04 10:15:47 +0800

    括号匹配

--- valid_parentheses/5.js
+++ valid_parentheses/5.js
@@ -14,6 +14,19 @@ function longestValidParentheses(s){
             left = right = 0;
         }
     }
+    left = right = 0;
+    for(var i = s.length-1;i >=0;i--){
+        if(s[i] == '('){
+            left++;
+        }else{
+            right++;
+        }
+        if(left == right){
+            max = Math.max(max,2*left);
+        }else if(right < left){
+            left = right = 0;
+        }
+    }
    return max;
 }
-console.log(longestValidParentheses('(())())('))
+console.log(longestValidParentheses('(())('))

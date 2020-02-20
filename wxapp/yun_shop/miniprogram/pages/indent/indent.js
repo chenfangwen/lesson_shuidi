7f060b5eb0413a1b97db7cc1f5d84e36a3774636
chenfangwen <2325189133@qq.com>
2020-02-20 16:16:25 +0800

    vue

--- wxapp/yun_shop/miniprogram/pages/indent/indent.js
+++ wxapp/yun_shop/miniprogram/pages/indent/indent.js
@@ -1,20 +1,71 @@
 // miniprogram/pages/indent/indent.js
+const db = wx.cloud.database(); //云数据库
+// const indentCollection = db.collection('indent'); //表 collection
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-
+  data: { 
+    isselect1: 1,
+    isselect2:1,
+    isselect2_2: 1,
+    isselect2_3: 1,
+    indent: [],
+    unuseindent: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    db.collection('indent').where({
+      isuse:'未使用'
+    }).get({
+      success(res) {
+        console.log('查寻成功', res.data);
+        that.setData({
+          unuseindent:res.data
+        })
+      }
+    })
+    db.collection('indent').get({
+      success(res) {
+        console.log('查寻成功', res.data);
+        that.setData({
+          indent: res.data
+        })
+      }
+    })
   },
-
+  select1(e){
+    let id = e.currentTarget.dataset.id;
+    this.setData({
+      isselect1:id
+    })
+  },
+  select2(e) {
+    let id = e.currentTarget.dataset.id2;
+    this.setData({
+      isselect2: id
+    })
+    // console.log(this.data.isselect2)
+  },
+  select2_2(e) {
+    let id = e.currentTarget.dataset.id2;
+    this.setData({
+      isselect2_2: id
+    })
+    // console.log(this.data.isselect2)
+  },
+  select2_3(e) {
+    let id = e.currentTarget.dataset.id2;
+    this.setData({
+      isselect2_3: id
+    })
+    // console.log(this.data.isselect2)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */

34bb0ffd7803831d800416e2c59d95cd43b818e9
chenfangwen <2325189133@qq.com>
2020-02-24 18:07:05 +0800

    组件开发

---
+++ vue/search-component/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <search/>
+    <router-view/>
+  </div>
+</template>
+<script>
+import Search from './components/Search'
+export default {
+  components:{
+    Search
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>

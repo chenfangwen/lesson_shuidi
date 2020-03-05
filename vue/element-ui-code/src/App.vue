f142f19b65682432a4cca69c292fd647c452425e
chenfangwen <2325189133@qq.com>
2020-03-05 09:36:47 +0800

    element-ui 源码分析

--- vue/element-ui-code/src/App.vue
+++ vue/element-ui-code/src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <el-alert
+      type="success"
+      title="成功提示"
+      :showIcon="true"
+      description="这是成功的一个提示"
+    >
+    </el-alert>
+     <el-alert
+      type="error"
+      title="错误提示"
+      :showIcon="true"
+      description="这是错误的一个提示"
+    >
+    </el-alert>
+     <el-alert
+      type="warn"
+      title="警告提示"
+      :showIcon="true"
+      description="这是警告的一个提示"
+    >
+    </el-alert>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ElAlert from './components/ElAlert.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ElAlert
   }
 }
 </script>

88ddf693db9550deca83f36edb90599231d784f6
chenfangwen <2325189133@qq.com>
2020-03-27 14:10:46 +0800

    vue-tree

--- node/node_get_json/index.js
+++ node/node_get_json/index.js
@@ -1,13 +1,11 @@
+const axios = require('axios');
+const fs = require('fs');
 
-let request = require('request-promise');
-let cheerio = require('cheerio');
-let fs=require('fs');
-  // 进程
-  let html =  request({
-    url: 'https://raw.githubusercontent.com/lutangar/cities.json/master/cities.json'
-  });
-  // console.log(html)
-  let $ = cheerio.load(html);
-  console.log($)
-  let pre = $('body').find('pre').text();
-  console.log(pre)
+axios({
+    method:'get',
+    url:'https://raw.githubusercontent.com/lutangar/cities.json/master/cities.json',
+    responseType:'stream'
+  })
+  .then(function (response) {
+        response.data.pipe(fs.createWriteStream('cities.json'))
+});

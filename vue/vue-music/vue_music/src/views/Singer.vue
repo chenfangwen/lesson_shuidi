22c605d43f2386c2f1fcf5e81a7e88179566630d
chenfangwen <2325189133@qq.com>
2020-04-03 13:34:32 +0800

    歌手详情

--- vue/vue-music/vue_music/src/views/Singer.vue
+++ vue/vue-music/vue_music/src/views/Singer.vue
@@ -2,16 +2,56 @@
     <div class="" id="singer">
         <div class="header">
             <img @click="back" src="../assets/back.png" alt="">
+            <div class="name">{{singerInfo.name}}</div>
         </div>
         <div class="bgimg">
             <img :src="singerInfo.picUrl" alt="">
         </div>
         <div class="content">
             <van-tabs v-model="active" sticky animated swipeable>
-                <van-tab title="主页">内容 1</van-tab>
-                <van-tab title="歌曲">内容 2</van-tab>
+                <van-tab title="主页">
+                    <div class="info">
+                        <div class="someHot">
+                            <div class="hot">近期热门</div>
+                            <div @click="getCur_music_m(item,index)" class="song" v-for="(item,index) in someHot" :key="index">
+                                <div class="index">
+                                    <img :src="item.al.picUrl" alt="">
+                                </div>
+                                <div class="info">
+                                    <div class="name">
+                                        <div class="name1">{{item.name}}</div>
+                                        <div class="name2" v-if="item.alia!=''">({{item.alia[0]}})</div>
+                                    </div>
+                                    <div class="singer">{{item.ar[0].name}}-{{item.al.name}}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="baseInfo">
+                            <div class="text">基本信息</div>
+                            <div class="content">
+
+                            </div>
+                        </div>
+                    </div>
+                </van-tab>
+                <van-tab title="歌曲">
+                    <div class="hotSongs">
+                        <div class="list">
+                            <div @click="getCur_music_m(item,index)" class="song" v-for="(item,index) in hotSongs" :key="index">
+                                <div class="index">{{index+1}}</div>
+                                <div class="info">
+                                    <div class="name">
+                                        <div class="name1">{{item.name}}</div>
+                                        <div class="name2" v-if="item.alia!=''">({{item.alia[0]}})</div>
+                                    </div>
+                                    <div class="singer">{{item.ar[0].name}}-{{item.al.name}}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </van-tab>
                 <van-tab title="专辑">内容 3</van-tab>
-                <van-tab title="视频">内容 4</van-tab>
+                <van-tab title="视频">暂无视频</van-tab>
             </van-tabs>
         </div>
         <!-- <div class="test">sfs</div> -->
@@ -20,12 +60,14 @@
 
 <script>
 import singer from '../api/singer.js'
+import {mapActions} from 'vuex'
 export default {
     data() {
         return {
             singerInfo:{},
             hotSongs:[],
-            active:1
+            active:1,
+            someHot:[]
         }
     },
     watch:{
@@ -35,6 +77,12 @@ export default {
         }
     },
     methods:{
+        ...mapActions(['getCur_music','getCurIndex','getCurList']),
+        getCur_music_m(item,index){
+            this.getCur_music(item)
+            this.getCurIndex(index),
+            this.getCurList(this.hotSongs)
+        },
         search(){
             window.scrollTo(0,0)
             console.log(this.$route.params)
@@ -46,6 +94,7 @@ export default {
                     this.singerInfo = res.data.artist
                     // this.singerPic = this.singerInfo.picUrl
                     this.hotSongs = res.data.hotSongs
+                    this.someHot = res.data.hotSongs.slice(0,3)
                 }
             })
         },
@@ -63,15 +112,17 @@ export default {
 #singer{
     position absolute
     width 100%
-    height 300px
-    z-index 2
+    height 100%
+    z-index 100
     background-color #fff
     overflow scoll
     .header{
         position fixed
+        display flex
         top 0
         width 100%
-        height 30px
+        height 50px
+        background-color  rgb(212, 68, 57);
         text-align left
         z-index 2
         img{
@@ -80,29 +131,152 @@ export default {
             margin-left 10px
             margin-top 10px
         }
+        .name{
+            color #fff
+            font-size 18px
+            font-weight 600
+            line-height 50px
+        }
     }
     .bgimg{
         position absolute
-        top 0
+        top 20px
         height 43%
         width 100%
         background-size cover
         img{
             position relative
             display inline-block
-            max-width 100%
+            width 100%
             height auto
         }
     }
     .content{
-        position sticky
-        margin-top 250px
-        top 40px
+        position relative
+        margin-top 70%
+        z-index 1
+        height 100%
         border-radius 15px 15px 0 0
         background-color #fff
         width 100%
-        z-index 100
-        height 1000px
+        .info{
+            width 100vw
+            margin-top 5px
+            .someHot{
+                position relative
+                margin-left 3%
+                width 94%
+                .song{
+                    display flex
+                    position relative
+                    width 100%
+                    height 70px
+                    .index{
+                        margin-top 10px
+                        width 50px
+                        height 50px
+                        img{
+                            width 50px
+                            height 50px
+                        }
+                    }
+                    .info{
+                        text-align left
+                        margin-left 10px
+                        margin-top 10px
+                        height 50px
+                        width 70%
+                        .name{
+                            display flex
+                            font-size 15px
+                            width 100%
+                            height 30px
+                            line-height 30px
+                            .name1{
+                                overflow: hidden;
+                                text-overflow:ellipsis;//文本溢出显示省略号
+                                display: -webkit-box;
+                                -webkit-line-clamp: 1; //控制文字行数
+                                -webkit-box-orient: vertical; //子元素数值排列
+                            }
+                            .name2{
+                                color #757575; 
+                                margin-left 5px
+                                overflow: hidden;
+                                text-overflow:ellipsis;//文本溢出显示省略号
+                                display: -webkit-box;
+                                -webkit-line-clamp: 1; //控制文字行数
+                                -webkit-box-orient: vertical; //子元素数值排列
+                            }
+                        }
+                        .singer{
+                            font-size 12px
+                            height 20px
+                            line-height 20px
+                            color #757575; 
+                        }
+                    }
+                }
+            }
+        }
+        .hotSongs {
+            width 100vw
+            .list {
+                position relative
+                margin-left 3%
+                width 94%
+                .song{
+                    display flex
+                    position relative
+                    width 100%
+                    height 70px
+                    .index{
+                        width 50px
+                        height 50px
+                        text-align center
+                        line-height 50px
+                        margin-top 10px
+                        color #757575;
+                    }
+                    .info{
+                        text-align left
+                        margin-left 10px
+                        margin-top 10px
+                        height 50px
+                        width 70%
+                        .name{
+                            display flex
+                            font-size 15px
+                            width 100%
+                            height 30px
+                            line-height 30px
+                            .name1{
+                                overflow: hidden;
+                                text-overflow:ellipsis;//文本溢出显示省略号
+                                display: -webkit-box;
+                                -webkit-line-clamp: 1; //控制文字行数
+                                -webkit-box-orient: vertical; //子元素数值排列
+                            }
+                            .name2{
+                                color #757575; 
+                                margin-left 5px
+                                overflow: hidden;
+                                text-overflow:ellipsis;//文本溢出显示省略号
+                                display: -webkit-box;
+                                -webkit-line-clamp: 1; //控制文字行数
+                                -webkit-box-orient: vertical; //子元素数值排列
+                            }
+                        }
+                        .singer{
+                            font-size 12px
+                            height 20px
+                            line-height 20px
+                            color #757575; 
+                        }
+                    }
+                }
+            }
+        }
     }
     .test{
         margin-top 1000px

5cf522201e0693e88b1687f57ceb4ca04baeae09
chenfangwen <2325189133@qq.com>
2020-04-06 11:55:16 +0800

    tab

---
+++ vue/vue-qiaochu-shop/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <m-header/>
+  </div>
+</template>
+
+<script>
+import mHeader from './components/m-header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    mHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

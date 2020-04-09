ae2192f6435cb6248d4c379a11dce1b38d9d47db
chenfangwen <2325189133@qq.com>
2020-04-09 21:29:21 +0800

    react 函数式编程

--- react/react-hoc/src/App.js
+++ react/react-hoc/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class A extends React.Component {
+  render() {
+    const {x,y} = this.props;
+    return (
+      <div>
+        x: {x} - y: {y}
+      </div>
+    )
+  }
+}
+class B extends React.Component {
+  render() {
+    const {x,y} = this.props;
+    return (
+      <div>
+        <h1>x: {x} - y: {y}</h1>
+      </div>
+    )
+  }
+}
+
+const Axy = withMouseInfo(A)
+const Bxy = withMouseInfo(B)
+
+function withMouseInfo(Compoent) {
+
+  class MouseXy extends Component {
+    state = {
+      x:0,
+      y:0
+    }
+    componentDidMount() {
+      document.body.addEventListener('mousemove',(e) => {
+        // console.log(e.clientX,e.clientY)
+        this.setState({
+          x:e.clientX,
+          y:e.clientY
+        })
+      })
+    }
+    render() {
+      const {x,y} = this.state
+      return(
+        <Compoent x={x} y={y} />
+      )
+    }
+  }
+  return MouseXy
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Axy></Axy>
+        <Bxy></Bxy>
+      </div>
+    )
+  }
 }
 
 export default App;

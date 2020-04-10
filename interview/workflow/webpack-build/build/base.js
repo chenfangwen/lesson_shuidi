df0850eef581a11af1508f18d3ac8b666eeb09c6
chenfangwen <2325189133@qq.com>
2020-04-10 19:36:27 +0800

    webpack架构

--- interview/workflow/webpack-build/build/base.js
+++ interview/workflow/webpack-build/build/base.js
@@ -1,23 +1,23 @@
 const { findSync } = require('../lib')
 const Config = require('webpack-chain');
-const config = new Config();    //配置实例
+const config = new Config();    //webpack-chain配置实例
 const path = require('path')
 const resolve = (src) => {
-    return path.join(process.cwd(),src)
+    return path.join(process.cwd(),src)     //定位项目下文件
 }
-const  files = findSync('config');
+const  files = findSync('config');      //遍历src/config webpack配置的所有文件
 console.log(files,"++++")
 
 module.exports = () => {
     const map = new Map()   //es6 Map key不限类型
-    files.map(file => {
-        const name = file.split('/').pop().replace('.js','')
+    files.map(file => {        //遍历config下的文件
+        const name = file.split('/').pop().replace('.js','')  //获得文件路径(去掉后缀)
         // console.log(name,'---')
-        return map.set(name,require(file)(config,resolve))
+        return map.set(name,require(file)(config,resolve))      //将文件输出的函数拿到，并传递参数，作为值放进map里
     })
     // console.log(map,'map-----')
-    map.forEach(v => {
-        v()
+    map.forEach(v => {      //遍历map的值，即三个配置函数，
+        v()         //执行，配置config
     })
-    return config
+    return config       //返回配置好了的config
 }
--- interview/workflow/webpack-build/build/build.js
+++ interview/workflow/webpack-build/build/build.js
@@ -1,31 +1,32 @@
-const rimraf = require('rimraf')
+const rimraf = require('rimraf')    //删除文件的包
 const path = require('path')
 const ora = require('ora')  //加载指示器
-const chalk = require('chalk')
+const chalk = require('chalk')  
 // webpack 最基本的 entry output mode
 // webpack-chain 编程
 // console.log(process.cwd(),__dirname) 项目的根目录下 webpack-build
 
-const config = require('./base')()
+const config = require('./base')()     //拿到base.js 输出函数的返回值config
 // console.log(config)
 const webpack = require('webpack')
 
-rimraf.sync(path.join(process.cwd(),'dist'))
+rimraf.sync(path.join(process.cwd(),'dist'))  //打包之前删除原文件
 
-const spiner = ora('开始构建项目...')
+const spiner = ora('开始构建项目...')       //打包过程提示
 spiner.start()
 
-webpack(config.toConfig(),function(err,status){
-    spiner.stop()
-    if(err) throw err
-    process.stdout.write(
-        status.toString({
-            colors: true,
-            modules: false,
-            children: false,
-            chunks: false,
-            chunkModules: false
-        }) + '\n\n'  
-    )
-    console.log(chalk.cyan('build 完成\n'))
-})
+webpack(config.toConfig(),  //webpack 打包
+    function(err,status){     //打包完成
+        spiner.stop()       //取消打包提示
+        if(err) throw err       //错误处理
+        process.stdout.write(
+            status.toString({
+                colors: true,
+                modules: false,
+                children: false,
+                chunks: false,
+                chunkModules: false
+            }) + '\n\n'  
+        )
+        console.log(chalk.cyan('build 完成\n'))
+    })

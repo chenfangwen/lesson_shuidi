45340c5fb70b6afa0a0c64250194ba8c88519854
chenfangwen <2325189133@qq.com>
2020-04-21 18:21:18 +0800

    优美数组

---
+++ leetcode/numberOfSubarrays/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const numberOfSubarrays = (nums, k) => {
+    let n = nums.length, counts = new Array(n+1).fill(0).fill(1, 0, 1), oddCount = 0, result = 0
+    console.log(counts)
+    for (let i = 0; i < n; i++) {
+      oddCount += nums[i] & 1
+      result += oddCount >= k ? counts[oddCount - k] : 0
+      counts[oddCount] += 1
+    }
+    return result
+  }

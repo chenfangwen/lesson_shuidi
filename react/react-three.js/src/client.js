0d6f970f6c000b75c30642383f3094df3407bfc5
chenfangwen <2325189133@qq.com>
2020-04-22 17:10:09 +0800

    three.js

---
+++ react/react-three.js/src/client.js
@@ -0,0 +1,12 @@
+// console.log('12312343');
+// var a = 1
+// document.body.innerHTML = a 
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import GameContainer from './containers/GameContainer'
+
+ReactDOM.render(
+    <GameContainer/>,
+    document.getElementById('game')
+)

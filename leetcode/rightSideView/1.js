5520f341152980855d1cbb66e1f653e5bb309933
chenfangwen <2325189133@qq.com>
2020-04-22 12:32:33 +0800

    从右边看数

---
+++ leetcode/rightSideView/1.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+  if(!root) return []
+  let arr = []
+  dfs(root, 0, arr)
+  return arr
+};
+function dfs (root, step, res) {
+  if(root){
+    if(res.length === step){
+      res.push(root.val)           // 当数组长度等于当前 深度 时, 把当前的值加入数组
+    }
+    console.log(step, '-------', res)
+    dfs(root.right, step + 1, res) // 先从右边开始, 当右边没了, 再轮到左边
+    dfs(root.left, step + 1, res)
+  }
+}

2179c2e87aae3f53353df0e79b87bcc0d22ca0a1
chenfangwen <2325189133@qq.com>
2020-04-24 19:41:04 +0800

    逆序对

---
+++ html5/laiwensite/2.js
@@ -0,0 +1,14 @@
+function LwstDP (a,b,n,m) {
+
+    for(var i=0;i<5;i++){        //一维长度为3
+        A[i] = new Array();
+        A[0][i] = i
+        A[i][0] = i
+    }
+    console.log(A)
+    // for(let i = 0; i < n; i++){ 
+
+    // }
+}
+
+LwstDP('asd','sds',3,3)

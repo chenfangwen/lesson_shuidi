7057a51b1e99bd359239b0f7eb368d9da6abc156
chenfangwen <2325189133@qq.com>
2020-04-27 20:19:21 +0800

    WPA

--- interview/PWA/sw.js
+++ interview/PWA/sw.js
@@ -1,1 +1,12 @@
 const CACHE_NAME = 'sw-' + 0;
+
+self.addEventListener('install',(event) => {
+    let cacheComplete = caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll([
+            './',   //根路径
+            './in.png'  //资源
+        ])
+    })
+
+    event.waitUntil()
+})

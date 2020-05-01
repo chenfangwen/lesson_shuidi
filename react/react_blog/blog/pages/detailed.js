5737a817085e87d6b59c24662f5b50434064da48
chenfangwen <2325189133@qq.com>
2020-05-01 19:59:53 +0800

    连接排序数组（递归）

--- react/react_blog/blog/pages/detailed.js
+++ react/react_blog/blog/pages/detailed.js
@@ -11,42 +11,29 @@ import '../static/style/pages/detailed.css'
 import MarkNav from 'markdown-navbar';
 import 'markdown-navbar/dist/navbar.css';
 
-const Detailed = (article) => {
-  console.log(article)
-  let markdown='# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(111)\` \n\n'+
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```'
+import marked from 'marked'
+import hljs from "highlight.js";
+import 'highlight.js/styles/monokai-sublime.css';
+
+const Detailed = (props) => {
+  console.log(props)
+  const renderer = new marked.Renderer();
+
+  marked.setOptions({
+    renderer: renderer, 
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code) {
+        return hljs.highlightAuto(code).value;
+    }
+  }); 
+
+  let html = marked(props.article_content)
   return (
     <>
       <Head>
@@ -66,20 +53,18 @@ const Detailed = (article) => {
 
              <div>
                 <div className="detailed-title">
-                React实战视频教程-技术胖Blog开发(更新08集)
+                  {props.title}
                 </div>
 
                 <div className="list-icon center">
-                  <span><Icon type="calendar" /> 2019-06-28</span>
+                  <span><Icon type="calendar" /> {props.addTime} </span>
                   <span><Icon type="folder" /> 视频教程</span>
-                  <span><Icon type="fire" /> 5498人</span>
+                  <span><Icon type="fire" /> {props.view_count}人</span>
                 </div>
 
-                <div className="detailed-content" >
-                    <ReactMarkdown 
-                      source={markdown} 
-                      escapeHtml={false}  
-                    />
+                <div className="detailed-content" 
+                dangerouslySetInnerHTML={{__html:html}}
+                >
                 </div>
 
              </div>
@@ -94,7 +79,7 @@ const Detailed = (article) => {
               <div className="nav-title">文章目录</div>
               <MarkNav
                 className="article-menu"
-                source={markdown}
+                source={html}
                 ordered={false}
               />
           </div>
@@ -106,6 +91,8 @@ const Detailed = (article) => {
    </>
   )
 }
+
+
 
 Detailed.getInitialProps = async(context)=>{
 

8a77acb3b859eafced30ec0d31891cfcca600a74
chenfangwen <2325189133@qq.com>
2020-05-03 14:03:46 +0800

    添加文章

--- react/react_blog/admin/src/pages/Login.js
+++ react/react_blog/admin/src/pages/Login.js
@@ -17,12 +17,12 @@ function Login(){
 
     return (
         <div className="login-div">
-            <Spin tip="Loading..." spinning={isLoading}>
-                <Card title="JSPang Blog  System" bordered={true} style={{ width: 400 }} >
+            <Spin tip="登录中..." spinning={isLoading}>
+                <Card title="吾心系依博客系统" bordered={true} style={{ width: 400 }} >
                     <Input
                         id="userName"
                         size="large"
-                        placeholder="Enter your userName"
+                        placeholder="请输入用户名"
                         prefix={<Icon type="user" style={{color:'rgba(0,0,0,.25)'}} />}
                         onChange={(e)=>{setUserName(e.target.value)}}
                     /> 
@@ -30,12 +30,12 @@ function Login(){
                     <Input.Password
                         id="password"
                         size="large"
-                        placeholder="Enter your password"
+                        placeholder="请输入密码"
                         prefix={<Icon type="key" style={{color:'rgba(0,0,0,.25)'}} />}
                         onChange={(e)=>{setPassword(e.target.value)}}
                     />     
                     <br/><br/>
-                    <Button type="primary" size="large" block onClick={checkLogin} > Login in </Button>
+                    <Button type="primary" size="large" block onClick={checkLogin} > 登录 </Button>
                 </Card>
             </Spin>
         </div>

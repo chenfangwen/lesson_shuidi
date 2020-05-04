5c3386d6cfa8de3d2816cfa3307d7e47e5eee8f7
chenfangwen <2325189133@qq.com>
2020-05-04 22:11:51 +0800

    跳跃游戏递归

--- leetcode/jump2/1.js
+++ leetcode/jump2/1.js
@@ -19,3 +19,7 @@ var jump = function(nums) {
 // 链接：https://leetcode-cn.com/problems/jump-game-ii/solution/45-tiao-yue-you-xi-ii-by-alexer-660/
 // 来源：力扣（LeetCode）
 // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+// 作者：Alexer-660
+// 链接：https://leetcode-cn.com/problems/jump-game-ii/solution/45-tiao-yue-you-xi-ii-by-alexer-660/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。

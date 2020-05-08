4de8b9a2c69b392e57251ca0425789b30a7eae77
chenfangwen <2325189133@qq.com>
2020-05-08 21:30:01 +0800

    MPA多页应用

---
+++ interview/MPA-多页应用/src/pay/index.js
@@ -0,0 +1,10 @@
+import ReactDom from 'react-dom';
+import React from 'react'
+function App () {
+  return (<h2>
+    支付
+  </h2>)
+}
+
+
+ReactDom.render(<App />, document.getElementById('app'))
--- interview/MPA-多页应用/src/promotion/index.js
+++ interview/MPA-多页应用/src/promotion/index.js
@@ -1,5 +1,5 @@
 import ReactDom from 'react-dom';
-
+import React from 'react'
 function App () {
   return (<h2>
     促销

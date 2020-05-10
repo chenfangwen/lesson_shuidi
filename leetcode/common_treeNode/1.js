073f5008209f969dea8b8ac6ce661ca13152621a
chenfangwen <2325189133@qq.com>
2020-05-10 20:18:32 +0800

    vue 指令

---
+++ leetcode/common_treeNode/1.js
@@ -0,0 +1,8 @@
+var lowestCommonAncestor = function(root, p, q) {
+    if (!root || root === p || root === q) return root;
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+    if ((left === p && right === q)
+        || (left === q && right === p)) return root;
+    return left || right;
+};

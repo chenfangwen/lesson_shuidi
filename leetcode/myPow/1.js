d70bca39ad4b3e4262c0e2ac7b0b98b7a5b6deeb
chenfangwen <2325189133@qq.com>
2020-05-11 21:23:06 +0800

    词法分析

---
+++ leetcode/myPow/1.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = (num, power) => {
+    if (power < 0) return 1 / (num * myPow(num, -(power + 1)))
+    if (power === 0) return 1
+    if (power === 1) return num
+    return power % 2 === 1 ?
+      num * myPow(num, power - 1) :
+      myPow(num * num, power / 2)
+  }
+  

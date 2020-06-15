3483f48c7d1ceebf38f1f2cdcc2646948cf44ae1
chenfangwen <2325189133@qq.com>
2020-06-15 10:20:51 +0800

    bfc浮动

--- react/use-effect/src/App.js
+++ react/use-effect/src/App.js
@@ -6,11 +6,11 @@ import './App.css';
 function Counter() {
   const [count, setCount] = useState(0)
   const [songList, setSongList] = useState([])
-  const _c = useRef()   // () => return {current: }
+  const _c = useRef()   // () => return {current: } 
   _c.current = count
   function handleShow() {
     setTimeout(() => {
-      console.log(_c.current );
+      console.log(_c.current);
     }, 3000);
   }
   function handleAdd() {
@@ -33,11 +33,59 @@ function Counter() {
   )
 }
 
+class ProfilePage extends React.Component {
+  //参数传递才实现 保存事件触发当时的状态 
+  state = {
+    count: 0
+  }
+  showMessage = (count) => alert('Followed ' + count);
+
+  handleClick = () => {
+    const {count} = this.state;
+    setTimeout(() => this.showMessage(count), 3000);
+  };
+  add = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  render() {
+    return (
+      <>
+        <button onClick={this.handleClick}>show</button>
+        <button onClick={this.add}>+</button>
+      </>
+    )
+  }
+}
+
+function MessageThread() {
+  const [message, setMessage] = useState('');
+
+  const showMessage = () => {
+    alert('You said: ' + message);
+  };
+
+  const handleSendClick = () => {
+    setTimeout(showMessage, 3000);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  return <>
+    <input value={message} onChange={handleMessageChange} />
+    <button onClick={handleSendClick}>Send</button>
+  </>;
+}
 
 function App() {
   return (
     <div className="App">
       <Counter/>
+      <ProfilePage />
+      <MessageThread/>
     </div>
   );
 }

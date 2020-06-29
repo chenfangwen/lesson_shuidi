84548718b6547a77e874a66ef3269067feed59d5
chenfangwen <2325189133@qq.com>
2020-06-29 21:15:16 +0800

    es7-9

---
+++ interview/es7~es9/8.padStart.padEnd.js
@@ -0,0 +1,7 @@
+let date = new Date();
+let m = date.getMonth().toString();
+console.log(m)
+// YYYY-MM-DD
+// 2020-06-29
+console.log(m.padStart(2, '0'))
+
---
+++ interview/es7~es9/8.尾逗号.js
@@ -0,0 +1,10 @@
+// es-lint: 
+let obj = {
+  a: 1,
+  b: 2,
+}
+let arr = [1, 2, 3, 4,]
+console.log(arr);
+let arr1 = [1, 2, 3, 4,,,] // 稀疏数组
+console.log(arr1)
+// git

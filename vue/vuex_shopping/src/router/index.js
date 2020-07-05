d053b40e20abdfc3258b334c73fd90d69d2bdf0e
chenfangwen <2325189133@qq.com>
2020-07-05 20:50:11 +0800

    路由守卫

--- vue/vuex_shopping/src/router/index.js
+++ vue/vuex_shopping/src/router/index.js
@@ -46,6 +46,25 @@ const router = new VueRouter({
     base: process.env.BASE_URL,
     routes
 })
-  
+
+//  路由守卫
+// router.beforeEach((to,from,next)=>{
+//     if(to.matched.some(res=>res.meta.isLogin)){//判断当前路由及子路由是否需要登录
+//         if (sessionStorage['username']) {
+//             next();
+//         }else{
+//             next({
+//                 path:"/login",
+//                 query:{
+//                     redirect:to.fullPath
+//                 }
+//             });
+//         }
+
+//     }else{
+//         next()
+//     }
+// });
+
 export default router
   

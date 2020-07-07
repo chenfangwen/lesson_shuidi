5e90723ec2dbeb23c23a4442555889e80cf4e359
chenfangwen <2325189133@qq.com>
2020-07-07 20:41:03 +0800

    长列表渲染

--- interview/面试题笔记/异步发展/1.js
+++ interview/面试题笔记/异步发展/1.js
@@ -1,11 +1,11 @@
 async function test() {
-let a = await new Promise((reslove, reject) => {
-    setTimeout(() => {
-        console.log('等我')
-        reslove(1)
-    }, 1000);
-})
-console.log('1')
-console.log(a,'---')
+    let a = await new Promise((reslove, reject) => {
+        setTimeout(() => {
+            console.log('等我')
+            reslove(1)
+        }, 1000);
+    })
+    console.log('1')
+    console.log(a,'---')
 }
 test()
